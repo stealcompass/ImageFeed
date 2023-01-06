bbc711a1d71104c190d3c350d1f4b8b778f23f61
Veniamin Ermolaev <venya.e@gmail.com>
2023-01-06 15:03:53 +0300

    To check the homework

---
+++ ImageFeed/ImagesList/ImageListCell.swift
@@ -0,0 +1,7 @@
+//
+//  ImageListCell.swift
+//  ImageFeed
+//
+//  Created by Veniamin on 04.01.2023.
+//
+
---
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -0,0 +1,19 @@
+//
+//  ImagesListCell.swift
+//  ImageFeed
+//
+//  Created by Veniamin on 04.01.2023.
+//
+
+import Foundation
+import UIKit
+
+
+final class ImagesListCell: UITableViewCell {
+    
+    @IBOutlet weak var imageCell: UIImageView!
+    @IBOutlet weak var likeButton: UIButton!
+    @IBOutlet weak var dateLabel: UILabel!
+    static let reuseIdentifier = "ImagesListCell"
+    
+}
